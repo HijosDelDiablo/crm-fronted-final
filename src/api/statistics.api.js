15574b1fd1c274238f0f11bfb61bf0de5e91a96a
carlosPadillaTorres <carlos3.dp.t@gmail.com>
2025-11-29 17:11:21 -0600

    feat - api fetch factory implemented

--- src/api/statistics.api.js
+++ src/api/statistics.api.js
@@ -1,88 +1,30 @@
-import { fetchHeader } from "../utils/fetch-header.util";
+import { fetchApiGet } from "../utils/api-fetch-factory.util";
 
-export const getSalesReport = async(startDate,endDate, navigate) => {
-    const response = await fetch(import.meta.env.VITE_APP_API_URL + '/dashboard/reporte-ventas?startDate='
-        +startDate+'&endDate='+endDate, {
-            method: 'GET',
-            ...fetchHeader()
-        });
-        if(response.ok){
-            return response.json();
-        }else{
-            if(response.status === 401) navigate('/login');
-            console.error('Error al obtener el reporte de ventas');
-            return null;
-        }
+export const getSalesReport = async (startDate, endDate, navigate) => {
+    return await fetchApiGet('/dashboard/reporte-ventas?startDate='
+        + startDate + '&endDate=' + endDate, navigate, 'Error al obtener el reporte de ventas')
 };
 
-export const getTopProducts = async(navigate) => {
-    const response = await fetch(import.meta.env.VITE_APP_API_URL + '/dashboard/top-productos', {
-            method: 'GET',
-            ...fetchHeader()
-        });
-        if(response.ok){
-            const data = await response.json();
-            return data;
-        }else{
-            if(response.status === 401) navigate('/login');
-            console.error('Error al obtener el top de productos');
-            return null;
-        }
-    };
-    
-    export const getTopSellers = async(navigate) => {
-        const response = await fetch(import.meta.env.VITE_APP_API_URL + '/dashboard/top-vendedores', {
-            method: 'GET',
-            ...fetchHeader()
-        });
-        if(response.ok){
-            const data = await response.json();
-            return data;
-        }else{
-            if(response.status === 401) navigate('/login');
-            console.error('Error al obtener el top de vendedores');
-            return null;
-        }
+export const getTopProducts = async (navigate) => {
+    return await fetchApiGet('/dashboard/top-productos', navigate, 'Error al obtener el top de productos');
+};
+
+export const getTopSellers = async (navigate) => {
+    return await fetchApiGet('/dashboard/top-vendedores', navigate, 'Error al obtener el top de vendedores');
+};
+
+export const getTopSalesByPeriod = async (navigate) => {
+    return await fetchApiGet('/dashboard/ventas-periodo', navigate, 'Error al obtener ventas por periodo');
 }
 
-export const getFavoritesProducts = async(limit=100,year=2025,startWeek=1,endWeek=52,navigate) => {
-    const response = await fetch(import.meta.env.VITE_APP_API_URL + '/statistics/favorites/top/'+
-        '?limit='+limit+'&year='+year+'&startWeek='+startWeek+'&endWeek='+endWeek, {
-            method: 'GET',
-            ...fetchHeader()
-        });
-        if(response.ok){
-            if(response.status === 401) navigate('/login');
-            return response.json();
-        }else{
-            console.error('Error al obtener el los autos favoritos de los usuarios');
-            return null;
-        }
+export const getFavoritesProducts = async (limit = 100, year = 2025, startWeek = 1, endWeek = 52, navigate) => {
+    return await fetchApiGet('/statistics/favorites/top/' +
+        '?limit=' + limit + '&year=' + year + '&startWeek=' + startWeek + '&endWeek=' + endWeek, navigate, 'Error al obtener el los autos favoritos de los usuarios');
 };
-export const getFavoriteHistory = async(idProduct, navigate) => {
-    const response = await fetch(import.meta.env.VITE_APP_API_URL + '/statistics/favorite/history/'+idProduct, {
-            method: 'GET',
-            ...fetchHeader()
-        });
-        if(response.ok){
-            return response.json();
-        }else{
-            if(response.status === 401) navigate('/login');
-            console.error('Error al obtener el el historial de favoritos del auto');
-            return null;
-        }
+export const getFavoriteHistory = async (idProduct, navigate) => {
+    return await fetchApiGet('/statistics/favorite/history/' + idProduct, navigate, 'Error al obtener el el historial de favoritos del auto');
 };
 
-export const getSellerWithMoreActivity = async(navigate) => {
-    const response = await fetch(import.meta.env.VITE_APP_API_URL + '/statistics/seller-with-more-activity', {
-            method: 'GET',
-            ...fetchHeader()
-        });
-        if(response.ok){
-            return response.json();
-        }else{
-            if(response.status === 401) navigate('/login');
-            console.error('Error al obtener el vendedor con más actividad');
-            return null;
-        }
+export const getSellerWithMoreActivity = async (navigate) => {
+    return await fetchApiGet('/statistics/seller-with-more-activity', navigate, 'Error al obtener el vendedor con más actividad');
 };
---
+++ src/utils/api-fetch-factory.util.js
@@ -0,0 +1,29 @@
+import { fetchHeader } from "../utils/fetch-header.util";
+
+export const fetchApiGet = async (url, navigate, errorMessage) => {
+    const response = await fetch(import.meta.env.VITE_APP_API_URL + url, {
+        method: 'GET',
+        ...fetchHeader()
+    });
+    if (response.ok) {
+        return response.json();
+    } else {
+        if (response.status === 401) navigate('/login');
+        console.error(errorMessage);
+        return null;
+    }
+};
+
+export const fetchApiPost = async (url, navigate, errorMessage) => {
+    const response = await fetch(import.meta.env.VITE_APP_API_URL + url, {
+        method: 'POST',
+        ...fetchHeader()
+    });
+    if (response.ok) {
+        return response.json();
+    } else {
+        if (response.status === 401) navigate('/login');
+        console.error(errorMessage);
+        return null;
+    }
+};
