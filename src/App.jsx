6eee934f57dda7e616f219d744a47eed987c3c1c
CharlyPadilla <carlos3.dp.t@gmail.com>
2025-12-11 01:31:07 -0600

    fix-path Client/MisCotizaciones

--- src/App.jsx
+++ src/App.jsx
@@ -40,8 +40,8 @@ const ViewProducts = lazy(() => import("./pages/ViewProducts"));
 const ViewPurchases = lazy(() => import("./pages/ViewPurchases"));
 const MisCompras = lazy(() => import("./pages/Client/MisCompras"));
 const DetalleCompra = lazy(() => import("./pages/Client/DetalleCompra"));
-const MisPagos = lazy(() => import("./pages/client/MisPagos"));
-const MisCotizaciones = lazy(() => import("./pages/client/MisCotizaciones"));
+const MisPagos = lazy(() => import("./pages/Client/MisPagos"));
+const MisCotizaciones = lazy(() => import("./pages/Client/MisCotizaciones"));
 const MyProfile = lazy(() => import("./pages/MyProfile"));
 
 // Guards
